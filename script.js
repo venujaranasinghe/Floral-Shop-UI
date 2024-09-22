60e3a39c24dd4208be217e571ea7de7c5a00219c
venujaranasinghe <it23225510@my.sliit.lk>
2024-09-23 00:08:21 +0530

    updated js file for header section

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+document.querySelector('#search-btn').onclick = () => {
+    document.querySelector('.header').classList.toggle('search-active');
+    navbar.classList.remove('active');
+};
+
+
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick =() => {
+    navbar.classList.toggle('active');  
+    searchForm.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
